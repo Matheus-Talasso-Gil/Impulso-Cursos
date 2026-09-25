d65215b4da39a0df06750397132ef44a0cfc0f6c
Matheus Talasso Gil <matheustalasso@gmail.com>
2026-09-25 13:31:40 -0300

    Remove botão de atualizar do site

--- includes/header.php
+++ includes/header.php
@@ -5,7 +5,6 @@
         <a href="/mini_sistema/app/delete.php">Excluir</a>
         <a href="/mini_sistema/app/select.php">Relatório</a>
         <a href="/mini_sistema/app/select_w_w.php">Consultar</a>
-        <a href="/mini_sistema/app/update.php">Atualizar</a>
         <div>
             <a href="/mini_sistema/login/login.php">Entrar</a>
             <a href="/mini_sistema/login/logout.php">Sair</a>
